67cc23545403e5605cb4ff19f9c8f4eb644afba6
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2019-10-29 18:03:47 +0100

    snowtracks on a plane is working as intended

--- DAR/Assets/Scripts/FPS_Tracks.cs
+++ DAR/Assets/Scripts/FPS_Tracks.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPS_Tracks : MonoBehaviour
-{
+public class FPS_Tracks : MonoBehaviour {
     private Material _snowMaterial, _drawMaterial;
     private RenderTexture _splatmap;
     public Shader _drawShader;
@@ -18,7 +17,7 @@ public class FPS_Tracks : MonoBehaviour
     int _layerMask;
 
     // Start is called before the first frame update
-    void Start(){
+    void Start() {
         _layerMask = LayerMask.GetMask("Ground");
         _drawMaterial = new Material(_drawShader);
         //_drawMaterial.SetVector("_Color", Color.red);
@@ -30,19 +29,21 @@ public class FPS_Tracks : MonoBehaviour
         _snowMaterial = _terrain.GetComponent<MeshRenderer>().material;
         _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
         _snowMaterial.SetTexture("_Splat", _splatmap);
- 
+
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update() {
         if (Physics.Raycast(characterTransform.position, Vector3.down, out _groundHit, 1f, _layerMask)) {
-            _drawMaterial.SetVector("_Coordinate", new Vector4(_groundHit.textureCoord.x, _groundHit.textureCoord.y, 0, 0));
-            _drawMaterial.SetFloat("_Strength", _brushStrength);
-            _drawMaterial.SetFloat("_Size", _brushImpact);
-            RenderTexture tempTex = RenderTexture.GetTemporary(_splatmap.width, _splatmap.height, 0, RenderTextureFormat.ARGBFloat);
-            Graphics.Blit(_splatmap, tempTex);
-            Graphics.Blit(tempTex, _splatmap, _drawMaterial);
-            RenderTexture.ReleaseTemporary(tempTex);
+            if (_groundHit.collider.gameObject == this.gameObject) {
+                _drawMaterial.SetVector("_Coordinate", new Vector4(_groundHit.textureCoord.x, _groundHit.textureCoord.y, 0, 0));
+                _drawMaterial.SetFloat("_Strength", _brushStrength);
+                _drawMaterial.SetFloat("_Size", _brushImpact);
+                RenderTexture tempTex = RenderTexture.GetTemporary(_splatmap.width, _splatmap.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(_splatmap, tempTex);
+                Graphics.Blit(tempTex, _splatmap, _drawMaterial);
+                RenderTexture.ReleaseTemporary(tempTex);
+            }
         }
     }
 }
---
+++ DAR/Assets/Scripts/SculptingTerrain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SculptingTerrain : MonoBehaviour
+{
+    private float[,] startHeigthsMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startHeigthsMap = this.gameObject.GetComponent<Terrain>().terrainData.GetHeights(0, 0, 1, 1);
+        Debug.Log(startHeigthsMap);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
