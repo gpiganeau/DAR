596cea679fc1f2b549129bfd8dcaf306de0bb1cc
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2019-11-18 20:41:34 +0100

    Map appears on screen and is zoomable

---
+++ DAR/Assets/MenuAppearScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAppearScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void Update() {
+        if (Input.GetKeyDown("m")) {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+}
--- DAR/Assets/Scripts/mouseLook.cs
+++ DAR/Assets/Scripts/mouseLook.cs
@@ -8,24 +8,43 @@ public class mouseLook : MonoBehaviour
 
     public Transform playerBody;
 
-    float xRotation = 0f;   
+    float xRotation = 0f;
+    bool ignore;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        ignore = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (!ignore) {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+
+
+        if (Input.GetKeyDown("m")) {
+            Debug.Log("1");
+            if (Cursor.lockState == CursorLockMode.Locked) {
+                Debug.Log("2");
+                Cursor.lockState = CursorLockMode.None;
+                ignore = true;
+            }
+            else if (Cursor.lockState == CursorLockMode.None) {
+                Debug.Log("3");
+                Cursor.lockState = CursorLockMode.Locked;
+                ignore = false;
+            }
+        }
     }
 }
