725d4e7681233103fefa1989eb94792dd85fba61
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2020-01-20 17:41:47 +0100

    Le phoenix renait de ses cendres

---
+++ DAR/Assets/Scripts/Interaction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    float time;
+    float colorValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact() {
+        while (time < 1) {
+            colorValue = Mathf.Lerp(5, 95, time);
+        }
+    }
+
+}
