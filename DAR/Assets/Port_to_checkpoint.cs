afb458b4a3e17e9a6a861c76f859f6b9f824bfd2
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2020-01-22 17:34:30 +0100

    muko merge prep

---
+++ DAR/Assets/Port_to_checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Port_to_checkpoint : MonoBehaviour
+{
+    [SerializeField] private Transform[] checkpoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.F1)) {
+            GameObject.Find("unitychan").transform.position = checkpoints[0].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.F2)) {
+            GameObject.Find("unitychan").transform.position = checkpoints[1].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.F3)) {
+            GameObject.Find("unitychan").transform.position = checkpoints[2].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.F4)) {
+            GameObject.Find("unitychan").transform.position = checkpoints[3].position;
+        }
+        else if (Input.GetKeyDown(KeyCode.F5)) {
+            GameObject.Find("unitychan").transform.position = checkpoints[4].position;
+        }
+    }
+}
