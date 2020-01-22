33742caadc58415916ba5d5c6951207244bed2a8
Muko Vunda <mouser724@gmail.com>
2020-01-22 17:35:54 +0100

    progress muko

---
+++ DAR/Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CameraController))]
+public class InterfaceController : MonoBehaviour
+{
+    public CameraController cameraControl;
+    public Transform[] panels;
+    Transform currentPanel;
+
+    void Start()
+    {
+        currentPanel = panels[0];
+    }
+
+    void Update()
+    {
+        if (cameraControl.currentView == cameraControl.views[0]
+        && currentPanel != panels[0])
+        {
+            currentPanel.gameObject.SetActive(false);
+            currentPanel = panels[0];
+            currentPanel.gameObject.SetActive(true);
+        }
+        if (cameraControl.currentView == cameraControl.views[1]
+        && currentPanel != panels[1])
+        {
+            currentPanel.gameObject.SetActive(false);
+            currentPanel = panels[1];
+            currentPanel.gameObject.SetActive(true);
+        }
+        if (cameraControl.currentView == cameraControl.views[2]
+        && currentPanel != panels[2])
+        {
+            currentPanel.gameObject.SetActive(false);
+            currentPanel = panels[2];
+            currentPanel.gameObject.SetActive(true);
+        }
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
