d5deb55825bf9aa8bf5c6fef4c94959549d070b5
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2020-01-23 11:12:16 +0100

    Pre Merge cinematic

--- DAR/Assets/remove_snow_particle.cs
+++ DAR/Assets/remove_snow_particle.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class remove_snow_particle : MonoBehaviour
 {
+    GameObject Snowparticle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Snowparticle = GameObject.Find("Snow_Particles_New");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Snowparticle.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Snowparticle.SetActive(true);
     }
 }
