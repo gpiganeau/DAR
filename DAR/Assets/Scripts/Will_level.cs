176e82045cf3103975a05431616f3792c446113e
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2020-02-03 17:02:37 +0100

    Décor en static et builds antérieures supprimées

---
+++ DAR/Assets/Scripts/Will_level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Will_level : MonoBehaviour
+{
+    float current_level;
+    float max_level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMaxLevel(float level) {
+        max_level = level;
+    }
+
+}
