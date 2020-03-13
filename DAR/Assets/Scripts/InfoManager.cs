32b32121c5a5345fb1d7c3fed2b3dd87185ee74c
Muko Vunda <mouser724@gmail.com>
2020-03-13 16:08:09 +0100

    Feedback via texte (rough)

---
+++ DAR/Assets/Scripts/InfoManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoManager : MonoBehaviour
+{
+    public GameObject infoPanel;
+    public Text infoText;
+
+    void Start()
+    {
+        HideInfo();
+    }
+
+    IEnumerator ShowInfoCoroutine(string info, float timer){
+        float infoTimer = timer;
+        infoText.text = info;
+        while ( infoTimer > 0 )
+        {
+            infoTimer -= Time.deltaTime;
+            yield return null;
+        }
+        HideInfo();
+    }
+
+    public void ShowInfo(string info)
+    {
+        StartCoroutine(ShowInfoCoroutine(info, 3f));
+    }
+
+    public void HideInfo()
+    {
+        infoText.text = "";
+    }
+}
