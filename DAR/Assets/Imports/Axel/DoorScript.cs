3962fc821def645020c6a344838cb8a4a3b8a026
Muko Vunda <mouser724@gmail.com>
2020-04-11 15:42:39 +0200

    Son de la porte intégré + animation main menu plus rapide

--- DAR/Assets/Imports/Axel/DoorScript.cs
+++ DAR/Assets/Imports/Axel/DoorScript.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public Animator animatorDoor;
+
+    [FMODUnity.EventRef]
+    public string doorEvent = "";
+    public FMOD.Studio.EventInstance doorSound;
+
     void Start()
     {
         animatorDoor = gameObject.GetComponent<Animator>();
+        doorSound = FMODUnity.RuntimeManager.CreateInstance(doorEvent);
+        doorSound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform));
     }
     
     void Update()
@@ -19,11 +26,17 @@ public class DoorScript : MonoBehaviour
         if (!animatorDoor.GetBool("isOpen"))
         {
             animatorDoor.SetBool("isOpen", true);
+            
+            doorSound.setParameterByName("door_Open", 1);
+            doorSound.start();
         }
 
         else
         {
             animatorDoor.SetBool("isOpen", false);
+
+            doorSound.setParameterByName("door_Open", 0);
+            doorSound.start();
         }
     }
 
