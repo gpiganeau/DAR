a696985b66c20a8233e22cdc58f1dc96350df0bb
Muko Vunda <mouser724@gmail.com>
2020-04-11 17:25:50 +0200

    UI fixes + commentaires fmod

--- DAR/Assets/LoadingScreenAssets/Scripts/MainMenuToLoadScene.cs
+++ DAR/Assets/LoadingScreenAssets/Scripts/MainMenuToLoadScene.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuToLoadScene : MonoBehaviour
 {
     public int sceneToLoad;
+    public Image black;
+    public Animator animBlack;
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
@@ -13,13 +16,13 @@ public class MainMenuToLoadScene : MonoBehaviour
     
     public void GoToLoad()
     {
-        //StartCoroutine(Timer());
-        SceneManager.LoadScene(1);
+        StartCoroutine(Fading());
     }
 
-    private IEnumerator Timer()
+    private IEnumerator Fading()
     {
-        yield return new WaitForSeconds(0.5f);
+        animBlack.SetBool("Fade", true);
+        yield return new WaitUntil(()=>black.color.a == 1);
         //Debug.Log("Wait over");
         SceneManager.LoadScene(1);
     }
