6f03e658587b1579757541c97a5d96200cbedcbe
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2020-05-14 16:43:06 +0200

    On peut pêcher maggle et ça marche tout comme il faut. Plus qu'à faire de la UI par dessus

--- DAR/Assets/FishingScript.cs
+++ DAR/Assets/FishingScript.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class FishingScript : MonoBehaviour
 {
+    public Item fish1;
+    public Item fish2;
+    public Item fish3;
+
+    [SerializeField] PlayerInventoryManager PlayerInventoryManager;
+
     Coroutine fishingCoroutine;
-    PlayerInventoryManager PlayerInventoryManager;
+    Coroutine catchingFishCoroutine;
+    float timer;
+    float randomOffsetTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,17 +27,75 @@ public class FishingScript : MonoBehaviour
     }
 
     public void StartFishing() {
-        fishingCoroutine = StartCoroutine(FishingCoroutine());
+        if (fishingCoroutine == null) {
+            fishingCoroutine = StartCoroutine(FishingCoroutine());
+        }
+        
     }
 
-    public void StopFishing(bool success) {
-        StopCoroutine(fishingCoroutine);
-        if (success) {
-
+    public void StopFishing(int success) {
+        timer = 0f;
+        randomOffsetTime = -1f;
+        if (success == 1) {
+            PlayerInventoryManager.AddItem(Instantiate(fish1));
         }
+        else if (success == 2) {
+            PlayerInventoryManager.AddItem(Instantiate(fish2));
+        }
+        else if (success == 3) {
+            PlayerInventoryManager.AddItem(Instantiate(fish3));
+        }
+        StopCoroutine(fishingCoroutine);
+        fishingCoroutine = null;
+    }
+
+    IEnumerator CatchFish() {
+        int success = 0;
+        int fish;
+        float pickUpTimer = 0f;
+        float timeLimit = -1f;
+        float randomFish = Random.value;
+        if (randomFish < 0.75) {
+            timeLimit = 3f;
+            fish = 1;
+        }
+        else if(randomFish < 0.97) {
+            timeLimit = 1.5f;
+            fish = 2;
+        }
+        else {
+            timeLimit = 0.7f;
+            fish = 3;
+        }
+        while (pickUpTimer < timeLimit) {
+            pickUpTimer += Time.deltaTime;
+            //afficher indicateur
+            if (Input.GetButtonDown("Interact")) {
+                success = fish;
+                break; 
+            }
+            else {
+                yield return null;
+            }
+            yield return null;
+        }
+        //hide indicateur
+        StopFishing(success);
     }
 
     IEnumerator FishingCoroutine() {
-        yield return null;
+        float timer = 0f;
+        while (timer < 9f) {
+            timer += Time.deltaTime;
+            if (randomOffsetTime < 0) {
+                randomOffsetTime = Random.value * 5f;
+                yield return null;
+            }
+            else if (timer > (3f + randomOffsetTime)) {
+                Debug.Log("Now");
+                yield return CatchFish();
+            }
+            yield return null;
+        }
     }
 }
