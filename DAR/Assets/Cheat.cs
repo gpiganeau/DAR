5e227af8f9d58e89d5c40cc14a221cb7f4ef1d7a
Grégoire Piganeau <gregoirepiganeau@hotmail.fr>
2020-05-27 18:22:35 +0200

    Et avec le cheat code qui marche

--- DAR/Assets/Cheat.cs
+++ DAR/Assets/Cheat.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Cheat : MonoBehaviour
 {
     bool inUse;
+    public Item[] radioPieces;
+    Transform spawnTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,6 +18,10 @@ public class Cheat : MonoBehaviour
     {
         if ((Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.X) && Input.GetKey(KeyCode.C)) && !inUse) {
             inUse = true;
+            
+            for (int i = 0; i<radioPieces.Length;i++) {
+                Instantiate<GameObject>(radioPieces[i].prefab, new Vector3(814.1f + i, 102.0405f, 845.8f), new Quaternion());
+            }
             Debug.Log("Cheater");
         }
         else if (!Input.anyKey) {
